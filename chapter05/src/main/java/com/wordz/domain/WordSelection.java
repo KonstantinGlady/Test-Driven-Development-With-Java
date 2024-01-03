f8beb5922554366502c92f5a3653a28750d19260
Glady <gladyi@hotmail.com>
2024-01-03 07:04:21 +0100

    reportsWordNotFound RED

---
+++ chapter05/src/main/java/com/wordz/domain/WordSelection.java
@@ -0,0 +1,11 @@
+package com.wordz.domain;
+
+public class WordSelection {
+    public WordSelection(WordRepository repository, RandomNumbers random) {
+
+    }
+
+    public String chooseRandomWord() {
+        return null;
+    }
+}
