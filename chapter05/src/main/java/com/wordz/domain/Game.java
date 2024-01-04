1b2448d6e14d44f275610cc9cb3c88476ade11f3
Glady <gladyi@hotmail.com>
2024-01-04 10:48:45 +0100

    startsNewGame RED 2

---
+++ chapter05/src/main/java/com/wordz/domain/Game.java
@@ -0,0 +1,15 @@
+package com.wordz.domain;
+
+public class Game {
+    public String getWord() {
+        return null;
+    }
+
+    public int getAttemptNumber() {
+        return 0;
+    }
+
+    public Player getPlayer() {
+        return null;
+    }
+}
--- chapter05/src/test/java/com/wordz/domain/NewGameTest.java
+++ chapter05/src/test/java/com/wordz/domain/NewGameTest.java
@@ -1,8 +1,22 @@
 package com.wordz.domain;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(MockitoExtension.class)
 public class NewGameTest {
+
+    @Mock
+    private GameRepository gameRepository;
+    @InjectMocks
+    private Wordz wordz;
 
     @Test
     void startsNewGame() {
@@ -10,5 +24,13 @@ public class NewGameTest {
         var wordz = new Wordz();
         var player = new Player();
         wordz.newGame(player);
+
+        var gameArgument = ArgumentCaptor.forClass(Game.class);
+        verify(gameRepository).create(gameArgument.capture());
+        var game = gameArgument.getValue();
+
+        assertThat(game.getWord()).isEqualTo("ARISE");
+        assertThat(game.getAttemptNumber()).isZero();
+        assertThat(game.getPlayer()).isSameAs(player);
     }
 }
