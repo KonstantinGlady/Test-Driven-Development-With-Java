2c28b4be7440fe2af3ace2e7dc9585e963904631
Glady <gladyi@hotmail.com>
2024-01-04 06:33:37 +0100

    selectRandomWord REFACT

--- chapter05/src/test/java/com/wordz/domain/WordSelectionTest.java
+++ chapter05/src/test/java/com/wordz/domain/WordSelectionTest.java
@@ -1,5 +1,6 @@
 package com.wordz.domain;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
@@ -19,13 +20,17 @@ public class WordSelectionTest {
     @Mock
     private RandomNumbers random;
 
-    @Test
-    void selectsRandomWord() {
-
+    @BeforeEach
+    void beforeEachTest() {
         when(repository.highestWordNumber())
                 .thenReturn(HIGHEST_WORD_NUMBER);
         when(repository.fetchWordByNumber(WORD_NUMBER_SHINE))
                 .thenReturn("SHINE");
+    }
+
+    @Test
+    void selectsRandomWord() {
+
         when(random.next(HIGHEST_WORD_NUMBER))
                 .thenReturn(WORD_NUMBER_SHINE);
 
