af5215314eb22c69e3c5d1d1f15a53de76737f3f
Glady <gladyi@hotmail.com>
2024-01-11 14:26:51 +0100

    fetchWord RED dataSource null

--- chapter05/src/test/java/com/wordz/adapters/db/WordRepositoryPostgresTest.java
+++ chapter05/src/test/java/com/wordz/adapters/db/WordRepositoryPostgresTest.java
@@ -2,15 +2,20 @@ package com.wordz.adapters.db;
 
 import com.github.database.rider.core.api.configuration.DBUnit;
 import com.github.database.rider.core.api.configuration.Orthography;
+import com.github.database.rider.core.api.connection.ConnectionHolder;
 import com.github.database.rider.junit5.api.DBRider;
 import org.junit.jupiter.api.Test;
+
+import javax.sql.DataSource;
 
 
 @DBRider
 @DBUnit(caseSensitiveTableNames = true, caseInsensitiveStrategy = Orthography.LOWERCASE)
 public class WordRepositoryPostgresTest {
 
+    private DataSource dataSource;
 
+    private ConnectionHolder connectionHolder = () -> dataSource.getConnection();
 
     @Test
     void fetchWord() {
