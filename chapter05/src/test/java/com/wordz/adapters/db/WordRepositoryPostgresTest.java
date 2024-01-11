4fd8a8cce37b52e668339dfd5cfa180f2cb49798
Glady <gladyi@hotmail.com>
2024-01-11 14:29:05 +0100

    fetchWord RED could not initialize dbrider datasource

--- chapter05/src/test/java/com/wordz/adapters/db/WordRepositoryPostgresTest.java
+++ chapter05/src/test/java/com/wordz/adapters/db/WordRepositoryPostgresTest.java
@@ -4,7 +4,9 @@ import com.github.database.rider.core.api.configuration.DBUnit;
 import com.github.database.rider.core.api.configuration.Orthography;
 import com.github.database.rider.core.api.connection.ConnectionHolder;
 import com.github.database.rider.junit5.api.DBRider;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.postgresql.ds.PGSimpleDataSource;
 
 import javax.sql.DataSource;
 
@@ -14,8 +16,18 @@ import javax.sql.DataSource;
 public class WordRepositoryPostgresTest {
 
     private DataSource dataSource;
-
     private ConnectionHolder connectionHolder = () -> dataSource.getConnection();
+
+    @BeforeEach
+    void setupConnection() {
+        var ds = new PGSimpleDataSource();
+        ds.setServerNames(new String[]{"localhost"});
+        ds.setDatabaseName("wordzdb");
+        ds.setCurrentSchema("public");
+        ds.setUser("ciuser");
+        ds.setPassword("ciPassword");
+        this.dataSource = ds;
+    }
 
     @Test
     void fetchWord() {
