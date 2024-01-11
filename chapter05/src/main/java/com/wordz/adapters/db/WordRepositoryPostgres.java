041fa8283ff7b117f9c8c3f9e7a44741b65ac6d2
Glady <gladyi@hotmail.com>
2024-01-11 16:20:44 +0100

    fetchWord GREEN

--- chapter05/src/main/java/com/wordz/adapters/db/WordRepositoryPostgres.java
+++ chapter05/src/main/java/com/wordz/adapters/db/WordRepositoryPostgres.java
@@ -1,17 +1,31 @@
 package com.wordz.adapters.db;
 
 import com.wordz.domain.WordRepository;
+import org.jdbi.v3.core.Jdbi;
 
 import javax.sql.DataSource;
 
 public class WordRepositoryPostgres implements WordRepository {
-    public WordRepositoryPostgres(DataSource dataSource) {
 
+    private static final String SQL_FETCH_WORD_BY_NUMBER =
+            "select word from word where word_number=:wordNumber";
+    private final Jdbi jdbi;
+
+    public WordRepositoryPostgres(DataSource dataSource) {
+        jdbi = Jdbi.create(dataSource);
     }
 
     @Override
     public String fetchWordByNumber(int number) {
-        return null;
+
+        var word = jdbi.withHandle((handle) -> {
+            var query = handle.createQuery(SQL_FETCH_WORD_BY_NUMBER);
+            query.bind("wordNumber", number);
+
+            return query.mapTo(String.class).one();
+        });
+
+        return word;
     }
 
     @Override
