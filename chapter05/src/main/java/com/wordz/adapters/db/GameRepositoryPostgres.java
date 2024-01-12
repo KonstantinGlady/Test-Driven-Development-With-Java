27d70be6b86be5cc024cf5ac4c787c3715061a1e
Glady <gladyi@hotmail.com>
2024-01-12 18:05:55 +0100

    storesNewGame GREEN

--- chapter05/src/main/java/com/wordz/adapters/db/GameRepositoryPostgres.java
+++ chapter05/src/main/java/com/wordz/adapters/db/GameRepositoryPostgres.java
@@ -3,19 +3,30 @@ package com.wordz.adapters.db;
 import com.wordz.domain.Game;
 import com.wordz.domain.GameRepository;
 import com.wordz.domain.Player;
+import org.jdbi.v3.core.Jdbi;
 
 import javax.sql.DataSource;
 
 public class GameRepositoryPostgres implements GameRepository {
-    private final DataSource dataSource;
+    private static final String SQL_INSERT_NEW_GAME_ROW =
+            "insert into game (player_name, word, attempt_number, is_game_over)" +
+                    "values(:playerName, :word, :attemptNumber, :isGameOver)";
+    private final Jdbi jdbi;
 
     public GameRepositoryPostgres(DataSource dataSource) {
-        this.dataSource = dataSource;
+        jdbi = Jdbi.create(dataSource);
     }
 
     @Override
     public void create(Game game) {
-
+        jdbi.useHandle(handle ->
+                handle.createUpdate(SQL_INSERT_NEW_GAME_ROW)
+                        .bind("playerName", game.getPlayer().getName())
+                        .bind("word", game.getWord())
+                        .bind("attemptNumber", game.getAttemptNumber())
+                        .bind("isGameOver", game.isGameOver())
+                        .execute()
+        );
     }
 
     @Override
