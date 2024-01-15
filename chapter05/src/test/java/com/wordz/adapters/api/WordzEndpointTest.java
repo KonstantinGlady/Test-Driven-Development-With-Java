51e81879707c7343ea7cb079efec7cdafc736a19
Glady <gladyi@hotmail.com>
2024-01-15 19:17:37 +0100

    startGame RED 2

--- chapter05/src/test/java/com/wordz/adapters/api/WordzEndpointTest.java
+++ chapter05/src/test/java/com/wordz/adapters/api/WordzEndpointTest.java
@@ -3,6 +3,9 @@ package com.wordz.adapters.api;
 import com.vtence.molecule.http.HttpStatus;
 import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 
 import static com.vtence.molecule.testing.http.HttpResponseAssert.assertThat;
@@ -11,9 +14,11 @@ import static com.vtence.molecule.testing.http.HttpResponseAssert.assertThat;
 public class WordzEndpointTest {
 
     @Test
-    void startGame() {
+    void startGame() throws IOException, InterruptedException {
 
-        HttpResponse res = null ;
+        var httpClient = HttpClient.newHttpClient();
+        HttpRequest req = null;
+        HttpResponse res = httpClient.send(req, HttpResponse.BodyHandlers.discarding());
         assertThat(res).hasStatusCode(HttpStatus.NO_CONTENT.code);
     }
 
