68d1c2e0883bbd2b90b50b23d6c851ddbe3812e7
Glady <gladyi@hotmail.com>
2024-01-15 20:27:08 +0100

    startGame REFACT

--- chapter05/src/test/java/com/wordz/adapters/api/WordzEndpointTest.java
+++ chapter05/src/test/java/com/wordz/adapters/api/WordzEndpointTest.java
@@ -4,6 +4,8 @@ import com.google.gson.Gson;
 import com.vtence.molecule.http.HttpStatus;
 import com.wordz.domain.Player;
 import com.wordz.domain.Wordz;
+import org.jetbrains.annotations.NotNull;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
@@ -23,25 +25,39 @@ import static org.mockito.Mockito.when;
 public class WordzEndpointTest {
 
     private static final Player PLAYER = new Player("player1");
+    private final HttpClient httpClient = HttpClient.newHttpClient();
 
     @Mock
     private Wordz mockWordz;
+    private WordzEndpoint endpoint;
+
+    @BeforeEach
+    void setUp() {
+        endpoint = new WordzEndpoint(mockWordz, "localhost", 8080);
+    }
 
     @Test
     void startGame() throws IOException, InterruptedException {
 
-        var endpoint = new WordzEndpoint(mockWordz, "localhost", 8080);
         when(mockWordz.newGame(PLAYER))
                 .thenReturn(true);
 
-        var httpClient = HttpClient.newHttpClient();
-        HttpRequest req = HttpRequest.newBuilder()
-                .uri(URI.create("http://localhost:8080/start"))
-                .POST(HttpRequest.BodyPublishers
-                        .ofString(new Gson().toJson(PLAYER)))
+        HttpRequest req = requestBuilder("start")
+                .POST(asJsonBody(PLAYER))
                 .build();
         HttpResponse res = httpClient.send(req, HttpResponse.BodyHandlers.discarding());
         assertThat(res).hasStatusCode(HttpStatus.NO_CONTENT.code);
+    }
+
+    @NotNull
+    private static HttpRequest.BodyPublisher asJsonBody(Object source) {
+        return HttpRequest.BodyPublishers
+                .ofString(new Gson().toJson(source));
+    }
+
+    private static HttpRequest.Builder requestBuilder(String path) {
+        return HttpRequest.newBuilder()
+                .uri(URI.create("http://localhost:8080/" + path));
     }
 
 }
