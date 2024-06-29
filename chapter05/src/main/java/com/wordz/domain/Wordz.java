55676b36f5fe476bc91f97cb701ee3dc3e79bad5
Glady <gladyi@hotmail.com>
2024-06-29 14:02:36 +0300

    NewGameTest cannotRestartGame

--- chapter05/src/main/java/com/wordz/domain/Wordz.java
+++ chapter05/src/main/java/com/wordz/domain/Wordz.java
@@ -1,5 +1,7 @@
 package com.wordz.domain;
 
+import java.util.Optional;
+
 public class Wordz {
     private final GameRepository gameRepository;
     private final WordSelection wordSelection;
@@ -11,18 +13,34 @@ public class Wordz {
     }
 
     public boolean newGame(Player player) {
+        var currentGame = gameRepository.fetchForPlayer(player);
+        if (isGameInProgress(currentGame)) {
+
+            return false;
+        }
         var word = wordSelection.chooseRandomWord();
         var game = new Game(player, word, 0, false);
         gameRepository.create(game);
         return true;
     }
 
-    public GuessResult assess(Player player, String guess) {
-        var game = gameRepository.fetchForPlayer(player).get();
+    private static boolean isGameInProgress(Optional<Game> currentGame) {
+        if (currentGame.isEmpty()) {
+            return false;
+        }
+        return !currentGame.get().isGameOver();
+    }
 
-        if (game.isGameOver()) {
+    public GuessResult assess(Player player, String guess) {
+        var game = gameRepository.fetchForPlayer(player);
+
+        if (!isGameInProgress(game)) {
             return GuessResult.ERROR;
         }
+        return calculateScore(guess, game.get());
+    }
+
+    private GuessResult calculateScore(String guess, Game game) {
         var score = game.attempt(guess);
 
         gameRepository.update(game);
--- chapter05/src/test/java/com/wordz/domain/NewGameTest.java
+++ chapter05/src/test/java/com/wordz/domain/NewGameTest.java
@@ -7,6 +7,8 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.Optional;
+
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.Mockito.verify;
@@ -16,6 +18,7 @@ import static org.mockito.Mockito.when;
 public class NewGameTest {
 
     private final Player PLAYER = new Player("player1");
+    private final String CORRECT_WORD = "ARISE";
     @Mock
     private GameRepository gameRepository;
     @Mock
@@ -28,14 +31,24 @@ public class NewGameTest {
     @Test
     void startsNewGame() {
 
-        givenWordToSelect("ARISE");
+        givenWordToSelect(CORRECT_WORD);
         wordz.newGame(PLAYER);
         var game = getGameInRepository();
 
-        assertThat(game.getWord()).isEqualTo("ARISE");
+        assertThat(game.getWord()).isEqualTo(CORRECT_WORD);
         assertThat(game.getAttemptNumber()).isZero();
         assertThat(game.getPlayer()).isSameAs(PLAYER);
     }
+
+    @Test
+    void cannotRestartNewGame() {
+
+        when(gameRepository.fetchForPlayer(PLAYER))
+                .thenReturn(Optional.of(Game.create(PLAYER, CORRECT_WORD)));
+        var success = wordz.newGame(PLAYER);
+        assertThat(success).isFalse();
+    }
+
 
     private Game getGameInRepository() {
         var gameArgument = ArgumentCaptor.forClass(Game.class);
